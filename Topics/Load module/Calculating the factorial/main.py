5de0d3446a554e6f9206bf0fb9d28a70c9468cce
Sâm <samntt@ahamove.com>
2021-08-27 15:58:01 +0700

    Update

---
+++ Topics/Load module/Calculating the factorial/main.py
@@ -0,0 +1,6 @@
+# don't modify this code or variable `x` may not be available
+x = int(input())
+
+# use factorial() here
+from math import factorial
+print(factorial(x))
---
+++ Topics/Random module/Beta distribution/main.py
@@ -0,0 +1,7 @@
+import random
+
+alpha = 0.9
+beta = 0.1
+random.seed(3)
+# call the function here
+print(random.betavariate(alpha, beta))
